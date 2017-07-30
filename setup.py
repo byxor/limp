2d2061ce5af536f4b2218bb54d1416598c96c32f
byxor <brandnewbyxor@gmail.com>
2017-07-30 01:21:16 +0100

    Upload to pypi

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+
+setup(
+    name = "limp",
+    packages = ["limp"],
+    license = "GPLv3",
+    version = "0.1",
+    description = "A lisp-like interpreter",
+    author = "Brandon Ibbotson",
+    author_email = "brandon.ibbotson2@mail.dcu.ie",
+    url = "https://www.github.com/byxor/limp"
+)
