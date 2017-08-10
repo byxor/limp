d411d513ff4a750a940cc8b120cd4782976ff0cb
byxor <brandnewbyxor@gmail.com>
2017-08-10 01:02:51 +0100

    Move evaluation tests to own module

---
+++ test/evaluation_test.py
@@ -0,0 +1,50 @@
+import limp.environments as environments
+import limp.evaluation as evaluation
+import limp.syntax_tree as syntax_tree
+import limp.tokens as tokens
+from unittest import TestCase
+
+
+class EvaluationTest(TestCase):
+
+    def setUp(self):
+        make_evaluatable = lambda source_code: syntax_tree.create_from(tokens.create_from(source_code))
+        environment = environments.create_standard()
+        self.run = lambda source_code: evaluation.execute(make_evaluatable(source_code), environment)
+
+    def test_literals(self):
+        data = [
+            ('10', 10),
+            ('3.5', 3.5)
+        ]
+        for literal, expected_result in data:
+            self.assertEqual(expected_result, self.run(literal))
+
+    def test_accessing_non_existent_variable_raises_NameError(self):
+        self.assertRaises(NameError, self.run, 'x')
+        self.assertRaises(NameError, self.run, 'y')
+        self.assertRaises(NameError, self.run, 'z')
+    
+    def test_defining_and_accessing_variables(self):
+        self.run('(define x 10)')
+        self.run('(define y 20)')
+        self.run('(define z 30)')
+        self.assertEqual(10, self.run('x'))
+        self.assertEqual(20, self.run('y'))
+        self.assertEqual(30, self.run('z'))
+
+    def test_standard_library_procedures(self):
+        data = [
+            ('(+ 1 1)', 2),
+            ('(+ 2 3)', 5),
+            ('(- 1 1)', 0),
+            ('(- 4 2)', 2),
+            ('(* 2 3)', 6),
+            ('(* 4 1)', 4),
+            ('(/ 4 2)', 2),
+            ('(/ 1 2)', 0.5),
+            ('(// 1 2)', 0),
+            ('(// 5 2)', 2)
+        ]
+        for source_code, expected_result in data:
+            self.assertEqual(expected_result, self.run(source_code))
--- test/limp_test.py
+++ test/limp_test.py
@@ -7,8 +7,8 @@ import limp.evaluation as evaluation
 from unittest import TestCase
 
 
-class MiscellaneousLimpTest(TestCase):
-
+class TokensTest(TestCase):
+            
     def test_tokenising_source_code(self):
         data = [
             ('()', ['(', ')']),
@@ -18,19 +18,6 @@ class MiscellaneousLimpTest(TestCase):
         for source_code, expected_tokens in data:
             self.assertEqual(expected_tokens, tokens.create_from(source_code))
 
-    def test_atomising_symbols(self):
-        data = [
-            ('abc', 'abc', types.Symbol),
-            ('def', 'def', types.Symbol),
-            ('123', 123, int),
-            ('456', 456, int),
-            ('1.2', 1.2, float),
-            ('2.4', 2.4, float)
-        ]
-        for symbol, expected_atom_value, expected_atom_type in data:
-            atom_ = atom.create_from(symbol)
-            self.assertEqual(atom_, expected_atom_value)
-            self.assertEqual(type(atom_), expected_atom_type)
 
 
 class SyntaxTreeBuilderTest(TestCase):
@@ -58,49 +45,4 @@ class SyntaxTreeBuilderTest(TestCase):
             )
             
 
-class EvaluationTest(TestCase):
-
-    def setUp(self):
-        environment = environments.create_standard()
-        self.run = lambda source_code: evaluation.execute(syntax_tree.create_from(tokens.create_from(source_code)), environment)
-
-    def test_constants(self):
-        data = [
-            ('10', 10),
-            ('3.5', 3.5)
-        ]
-        for literal, expected_result in data:
-            self.assertEqual(expected_result, self.run(literal))
-
-    def test_accessing_non_existent_variable_raises_NameError(self):
-        self.assertRaises(NameError, self.run, 'x')
-        self.assertRaises(NameError, self.run, 'y')
-        self.assertRaises(NameError, self.run, 'z')
-    
-    def test_defining_and_accessing_variables(self):
-        self.run('(define x 10)')
-        self.run('(define y 20)')
-        self.run('(define z 30)')
-        self.assertEqual(10, self.run('x'))
-        self.assertEqual(10, self.run('x'))
-        self.assertEqual(20, self.run('y'))
-        self.assertEqual(20, self.run('y'))
-        self.assertEqual(30, self.run('z'))
-        self.assertEqual(30, self.run('z'))
-
-    def test_standard_library_procedures(self):
-        data = [
-            ('(+ 1 1)', 2),
-            ('(+ 2 3)', 5),
-            ('(- 1 1)', 0),
-            ('(- 4 2)', 2),
-            ('(* 2 3)', 6),
-            ('(* 4 1)', 4),
-            ('(/ 4 2)', 2),
-            ('(/ 1 2)', 0.5),
-            ('(// 1 2)', 0),
-            ('(// 5 2)', 2)
-        ]
-        for source_code, expected_result in data:
-            self.assertEqual(expected_result, self.run(source_code))
         
