ed163fc074977e1f076b1d2d1b6257e429b6dfe2
byxor <brandnewbyxor@gmail.com>
2017-08-15 19:47:32 +0100

    Upgrade to version 0.2

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ setup(
     name="limp",
     packages=["limp"],
     license="GPLv3",
-    version="0.1",
-    description="A lisp-like interpreter",
+    version="0.2",
+    description="An interpreter for a lisp-flavoured language",
     author="Brandon Ibbotson",
     author_email="brandon.ibbotson2@mail.dcu.ie",
     url="https://www.github.com/byxor/limp"
