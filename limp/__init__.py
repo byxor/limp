8b3ed8cea27cfd3ebaab8e12dc1afd1d129a0e07
byxor <brandnewbyxor@gmail.com>
2017-08-15 22:28:43 +0100

    Allow optional environment to be provided when evaluating

--- limp/__init__.py
+++ limp/__init__.py
@@ -4,10 +4,12 @@ import limp.syntax_tree as SyntaxTree
 import limp.tokens as Tokens
 
 
-def evaluate(source_code):
+def evaluate(source_code, environment=None):
+    if environment is None:
+        environment = Environment.create_standard()
     return Evaluation.execute(
         SyntaxTree.create_from(
             Tokens.create_from(
                 source_code)),
-        Environment.create_standard()
+        environment
     )
--- test/user_friendliness_test.py
+++ test/user_friendliness_test.py
@@ -1,12 +1,29 @@
 import limp
+import limp.environment
 from unittest import TestCase
 
 
-class InterfaceTest(TestCase):
+class Tests(TestCase):
 
+    def setUp(self):
+        self.run = limp.evaluate
+    
     def test_that_code_can_be_evaluated_with_a_simple_import(self):
-        evaluate = limp.evaluate
-        self.assertEqual(10, evaluate('10'))
-        self.assertEqual(2, evaluate('(- 5 3)'))
-        self.assertEqual(100, evaluate('(* 50 2)'))
+        self.assertEqual(10, self.run('10'))
+        self.assertEqual(2, self.run('(- 5 3)'))
+        self.assertEqual(100, self.run('(* 50 2)'))
+
+    def test_that_clean_environment_is_created_automatically(self):
+        self.run('(define x 10)')
+        self.assertRaises(Exception, self.run, 'x')
+        
+    def test_that_environment_can_be_provided(self):
+        environment = limp.environment.create_standard()
+        run = lambda source_code: self.run(source_code, environment)
+        run('(define x 10)')
+        run('(define y 20)')
+        run('(define z 30)')
+        self.assertEqual(10, run('x'))
+        self.assertEqual(20, run('y'))
+        self.assertEqual(30, run('z'))
     
