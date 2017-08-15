3785ac50438b2e259de3a0afc9391b4f20067ce5
byxor <brandnewbyxor@gmail.com>
2017-08-15 22:07:05 +0100

    Refactor syntax tree module

--- limp/syntax_tree.py
+++ limp/syntax_tree.py
@@ -3,21 +3,36 @@ import limp.parentheses as Parentheses
 
 
 def create_from(tokens):
-    SYNTAX_ERROR_MESSAGE = '{} while building Abstract Syntax Tree'
-    if len(tokens) == 0:
-        raise SyntaxError(SYNTAX_ERROR_MESSAGE.format('Unexpected EOF'))
-    token = tokens.pop(0)
-    if token == Parentheses.OPEN:
-        return _create_sub_tree_from(tokens)
-    elif token == Parentheses.CLOSE:
-        raise SyntaxError(SYNTAX_ERROR_MESSAGE.format('Unexpected closing parenthesis'))
+    _assert_not_empty(tokens)
+    _assert_parentheses_are_matched(tokens)
+
+    first_token = tokens.pop(0)
+    token_begins_new_form = lambda: first_token == Parentheses.OPEN
+    
+    if token_begins_new_form():
+        return _create_form_from(tokens)
     else:
-        return Atom.create_from(token)
+        return Atom.create_from(first_token)
 
 
-def _create_sub_tree_from(tokens):
-    tree = []
+def _create_form_from(tokens):
+    create_syntax_tree_from = create_from
+    form = []
     while tokens[0] != Parentheses.CLOSE:
-        tree.append(create_from(tokens))
+        sub_tree = create_syntax_tree_from(tokens)
+        form.append(sub_tree)
     tokens.pop(0)
-    return tree
+    return form
+    
+
+def _assert_not_empty(tokens):
+    if len(tokens) == 0:
+        raise SyntaxError(_ERROR_MESSAGE.format('Unexpected EOF'))
+
+
+def _assert_parentheses_are_matched(tokens):
+    if tokens[0] == Parentheses.CLOSE:
+        raise SyntaxError(_ERROR_MESSAGE.format('Unexpected closing parenthesis'))
+
+
+_ERROR_MESSAGE = '{} while building Abstract Syntax Tree'
