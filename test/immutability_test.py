4a009e6073219c5487f99b4c7d8ddd3c1052375a
byxor <brandnewbyxor@gmail.com>
2017-10-02 01:47:05 +0100

    Clean list immutability test

--- test/immutability_test.py
+++ test/immutability_test.py
@@ -5,12 +5,12 @@ from nose.tools import assert_equal
 
 def test_lists_are_immutable():
     LIST_NAME = 'my-list'
+    LIST_VALUE = [1, 2, 3]
 
     environment = Environment.create_standard()
+    environment.define(LIST_NAME, LIST_VALUE)
+    
     run = lambda source_code: limp.evaluate(source_code, environment)
-
-    run(f'(define {LIST_NAME} (list 1 2 3))')
-    list_before = environment.resolve(LIST_NAME)
 
     run(f'(append {LIST_NAME} 1)')
     run(f'(concatenate {LIST_NAME} (list 1 2 3))')
@@ -22,6 +22,4 @@ def test_lists_are_immutable():
     run(f'(all-but-first {LIST_NAME})')
     run(f'(all-but-last {LIST_NAME})')
 
-    list_after = environment.resolve(LIST_NAME)
-
-    yield assert_equal, list_before, list_after
+    yield assert_equal, LIST_VALUE, environment.resolve(LIST_NAME)
