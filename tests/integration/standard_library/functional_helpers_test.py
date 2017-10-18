8c710c4b2c14a0137c747734b058b4d3235b6c29
byxor <brandnewbyxor@gmail.com>
2017-10-19 00:59:16 +0100

    Clean functional helpers tests

--- tests/integration/standard_library/functional_helpers_test.py
+++ tests/integration/standard_library/functional_helpers_test.py
@@ -1,25 +1,129 @@
 import tests.helpers as Helpers
+from tests.syntax import *
+from tests.standard_library import *
+
+
+TEMPORARY_NUMBERS = list_of(integer(1), integer(2), integer(3), integer(4))
+TEMPORARY_NUMBER = symbol("n")
 
 
 def test():
     Helpers.run_evaluation_test_on([
-        ('((curry + 10) 20)',     30),
-        ('((curry + 10 1 2) 20)', 33),
-        ('((curry map (function (n) (* n 2))) (list 1 2 3))', [2, 4, 6]),
-        ('((curry filter (function (n) (= n 1))) (list 1 2 3))', [1]),
+        (invoke(
+            invoke(
+                PARTIAL,
+                ADD,
+                integer(10)),
+            integer(20)),
+         30),
         
-        ("""
-         (chain 0
-           (function (n) (+ n 10))
-           (function (n) (// n 2)))
-         """, 5),
+        (invoke(
+            invoke(
+                PARTIAL,
+                MAP,
+                IS_EVEN),
+            TEMPORARY_NUMBERS),
+         [False, True, False, True]),
 
-        ("""
-         (chain (list 1 2 3 4 5 6 7 8 9 10)
-           (curry map    (function (n) (* n 2)))
-           (curry map    (function (n) (+ n 1)))
-           (curry map    (function (n) (- n 1)))
-           (curry filter (function (n) (= (% n 4) 0)))
-           (curry reduce +))
-         """, 60),
+        (invoke(
+            invoke(
+                PARTIAL,
+                ADD,
+                integer(10),
+                integer(1),
+                integer(2)),
+            integer(20)),
+         33),
+
+        (invoke(
+            invoke(
+                PARTIAL,
+                MAP,
+                function(
+                    [TEMPORARY_NUMBER],
+                    invoke(
+                        MULTIPLY,
+                        TEMPORARY_NUMBER,
+                        integer(2)))),
+            TEMPORARY_NUMBERS),
+         [2, 4, 6, 8]),
+
+        (invoke(
+            invoke(
+                PARTIAL,
+                FILTER,
+                function(
+                    [TEMPORARY_NUMBER],
+                    invoke(
+                        ARE_EQUAL,
+                        TEMPORARY_NUMBER,
+                        integer(1)))),
+            TEMPORARY_NUMBERS),
+         [1]),
+
+        (invoke(
+            CHAIN,
+            integer(0),
+            function(
+                [TEMPORARY_NUMBER],
+                invoke(
+                    ADD,
+                    TEMPORARY_NUMBER,
+                    integer(10))),
+            function(
+                [TEMPORARY_NUMBER],
+                invoke(
+                    INTEGER_DIVIDE,
+                    TEMPORARY_NUMBER,
+                integer(2)))),
+         5),
+
+        (invoke(
+            CHAIN,
+            TEMPORARY_NUMBERS,
+            invoke(
+                PARTIAL,
+                MAP,
+                function(
+                    [TEMPORARY_NUMBER],
+                    invoke(
+                        MULTIPLY,
+                        TEMPORARY_NUMBER,
+                        integer(2)))),
+            invoke(
+                PARTIAL,
+                MAP,
+                function(
+                    [TEMPORARY_NUMBER],
+                    invoke(
+                        ADD,
+                        TEMPORARY_NUMBER,
+                        integer(1)))),
+            invoke(
+                PARTIAL,
+                MAP,
+                function(
+                    [TEMPORARY_NUMBER],
+                    invoke(
+                        SUBTRACT,
+                        TEMPORARY_NUMBER,
+                        integer(1)))),
+            invoke(
+                PARTIAL,
+                FILTER,
+                function(
+                    [TEMPORARY_NUMBER],
+                    invoke(
+                        ARE_EQUAL,
+                        invoke(
+                            MODULO,
+                            TEMPORARY_NUMBER,
+                            integer(4)),
+                        integer(0)))),
+            invoke(
+                PARTIAL,
+                REDUCE,
+                ADD)),
+         12)
+
     ])
---
+++ tests/standard_library.py
@@ -0,0 +1,23 @@
+from tests.syntax import *
+
+# Functional-programming-related
+PARTIAL = symbol("curry")
+CHAIN =   symbol("chain")
+
+# List-related
+MAP =     symbol("map")
+FILTER =  symbol("filter")
+REDUCE =  symbol("reduce")
+
+# Math-related
+ADD =            symbol("+")
+SUBTRACT =       symbol("-")
+MULTIPLY =       symbol("*")
+INTEGER_DIVIDE = symbol("//")
+MODULO =         symbol("%")
+IS_EVEN =        symbol("even?")
+
+# Misc
+ARE_EQUAL = symbol("=")
+
+
