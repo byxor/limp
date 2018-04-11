dc554bd7a4d3808c8576e93cb736abd1e7c6ab2d
byxor <brandnewbyxor@gmail.com>
2018-04-12 00:15:15 +0100

    Release 0.8.1

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ setup(
     name="limp",
     packages=find_packages(exclude=['contrib', 'docs', 'test*']),
     license="GPLv3",
-    version="0.7.1",
+    version="0.8.1",
     description="A general purpose programming language",
     author="Brandon Ibbotson",
     author_email="brandon.ibbotson2@mail.dcu.ie",
     url="https://www.github.com/byxor/limp",
-    install_requires=['PyFunctional'],
+    install_requires=['PyFunctional', 'rply'],
 )
