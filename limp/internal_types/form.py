b3caa7ea5bf1eb50ef28ca1113a3d101f84f0bcc
Brandon Ibbotson <brandnewbyxor@gmail.com>
2018-04-23 15:32:40 +0100

    Remove new lines

--- limp/internal_types/form.py
+++ limp/internal_types/form.py
@@ -2,22 +2,18 @@ from limp.utils import *
 
 
 class Constructor:
-
     def __init__(self, contents, environment):
         self._contents = contents
         self._environment = environment
 
 
 class EvaluatesSafelyValidityChecker:
-
     def is_valid(self):
         return evaluates_safely(self)
 
 
 def create_keyword_validity_checker(expected_index=0):
-
     class KeywordValidityChecker:
-
         def is_valid(self):
             if self.__has_enough_contents():
                 return self._contents[expected_index] == self.__class__.KEYWORD
