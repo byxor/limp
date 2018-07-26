74d45dfcdcad4df2aa3be0ed6d88d0f79ea9510f
byxor <brandnewbyxor@gmail.com>
2018-07-26 21:02:59 +0100

    Remove if function

--- limp/standard_library/conditions.py
+++ limp/standard_library/conditions.py
@@ -1,19 +1,10 @@
-IF        = 'if'
 CONDITION = 'condition'
 
 
 def symbols():
     return {
-        IF:        _if,
         CONDITION: _condition,
     }
-
-
-def _if(condition, value_if_true, value_if_false=None):
-    if condition:
-        return value_if_true
-    else:
-        return value_if_false
 
 
 def _condition(entries):
