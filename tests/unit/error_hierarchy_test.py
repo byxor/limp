7e763a72cc0c9e397df80f39b2dca631d45c1fd4
byxor <brandnewbyxor@gmail.com>
2018-07-26 21:58:29 +0100

    Automate error hierarchy test

--- tests/unit/error_hierarchy_test.py
+++ tests/unit/error_hierarchy_test.py
@@ -1,18 +1,20 @@
-from limp.errors import *
+import inspect
+import limp.errors as Errors
 from nose.tools import assert_true
 
 
+PARENT_EXCEPTION = Errors.LimpError
+
+
 def test_all_exceptions_inherit_from_custom_parent():
-    expected_parent = LimpError
-    data = [
-        UndefinedSymbol,
-        RedefinedSymbol,
-        EmptyCode,
-        ExtraClosingParenthesis,
-        MissingClosingParenthesis,
-        UnnecessarySequentialEvaluator,
-        UnclosedString,
-        EmptyInvocation,
-    ]
-    for exception in data:
-        yield assert_true, issubclass(exception, expected_parent)
+    exceptions = _all_exceptions_in(Errors)
+    for exception in exceptions:
+        yield assert_true, issubclass(exception, PARENT_EXCEPTION)
+
+
+def _all_exceptions_in(module):
+    contents = [getattr(module, thing) for thing in dir(module)]
+    classes = [thing for thing in contents if inspect.isclass(thing)]
+    exceptions = [thing for thing in classes if issubclass(thing, Exception)]
+    return exceptions
+
