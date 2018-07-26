030fe5f1a0cda818fcaa36d8d52a6aef5a2781f0
byxor <brandnewbyxor@gmail.com>
2018-07-26 02:58:21 +0100

    Fix up more tests

--- tests/integration/standard_library/functional_helpers_test.py
+++ tests/integration/standard_library/functional_helpers_test.py
@@ -11,123 +11,122 @@ TEMPORARY_NUMBERS = list_of(integer(1), integer(2), integer(3), integer(4))
 TEMPORARY_NUMBER = symbol("n")
 
 
-def test():
-    Helpers.run_evaluation_test_on([
-        (invoke(
+t0 = Helpers.evaluation_fixture("test", [
+    (invoke(
+        invoke(
+            PARTIAL,
+            ADD,
+            integer(10)),
+        integer(20)),
+     30),
+    
+    (invoke(
+        invoke(
+            PARTIAL,
+            MAP,
+            IS_EVEN),
+        TEMPORARY_NUMBERS),
+     [False, True, False, True]),
+    
+    (invoke(
+        invoke(
+            PARTIAL,
+            ADD,
+            integer(10),
+            integer(1),
+            integer(2)),
+        integer(20)),
+     33),
+    
+    (invoke(
+        invoke(
+            PARTIAL,
+            MAP,
+            function(
+                [TEMPORARY_NUMBER],
+                invoke(
+                    MULTIPLY,
+                    TEMPORARY_NUMBER,
+                    integer(2)))),
+        TEMPORARY_NUMBERS),
+     [2, 4, 6, 8]),
+    
+    (invoke(
+        invoke(
+            PARTIAL,
+            FILTER,
+            function(
+                [TEMPORARY_NUMBER],
+                invoke(
+                    ARE_EQUAL,
+                    TEMPORARY_NUMBER,
+                    integer(1)))),
+        TEMPORARY_NUMBERS),
+     [1]),
+    
+    (invoke(
+        CHAIN,
+        integer(0),
+        function(
+            [TEMPORARY_NUMBER],
             invoke(
-                PARTIAL,
                 ADD,
-                integer(10)),
-            integer(20)),
-         30),
-
-        (invoke(
+                TEMPORARY_NUMBER,
+                integer(10))),
+        function(
+            [TEMPORARY_NUMBER],
             invoke(
-                PARTIAL,
-                MAP,
-                IS_EVEN),
-            TEMPORARY_NUMBERS),
-         [False, True, False, True]),
-
-        (invoke(
-            invoke(
-                PARTIAL,
-                ADD,
-                integer(10),
-                integer(1),
-                integer(2)),
-            integer(20)),
-         33),
-
-        (invoke(
-            invoke(
-                PARTIAL,
-                MAP,
-                function(
-                    [TEMPORARY_NUMBER],
-                    invoke(
-                        MULTIPLY,
-                        TEMPORARY_NUMBER,
-                        integer(2)))),
-            TEMPORARY_NUMBERS),
-         [2, 4, 6, 8]),
-
-        (invoke(
-            invoke(
-                PARTIAL,
-                FILTER,
-                function(
-                    [TEMPORARY_NUMBER],
-                    invoke(
-                        ARE_EQUAL,
-                        TEMPORARY_NUMBER,
-                        integer(1)))),
-            TEMPORARY_NUMBERS),
-         [1]),
-
-        (invoke(
-            CHAIN,
-            integer(0),
+                INTEGER_DIVIDE,
+                TEMPORARY_NUMBER,
+                integer(2)))),
+     5),
+    
+    (invoke(
+        CHAIN,
+        TEMPORARY_NUMBERS,
+        invoke(
+            PARTIAL,
+            MAP,
+            function(
+                [TEMPORARY_NUMBER],
+                invoke(
+                    MULTIPLY,
+                    TEMPORARY_NUMBER,
+                    integer(2)))),
+        invoke(
+            PARTIAL,
+            MAP,
             function(
                 [TEMPORARY_NUMBER],
                 invoke(
                     ADD,
                     TEMPORARY_NUMBER,
-                    integer(10))),
+                    integer(1)))),
+        invoke(
+            PARTIAL,
+            MAP,
             function(
                 [TEMPORARY_NUMBER],
                 invoke(
-                    INTEGER_DIVIDE,
+                    SUBTRACT,
                     TEMPORARY_NUMBER,
-                integer(2)))),
-         5),
+                    integer(1)))),
+        invoke(
+            PARTIAL,
+            FILTER,
+            function(
+                [TEMPORARY_NUMBER],
+                invoke(
+                    ARE_EQUAL,
+                    invoke(
+                        MODULO,
+                        TEMPORARY_NUMBER,
+                        integer(4)),
+                    integer(0)))),
+        invoke(
+            PARTIAL,
+            REDUCE,
+            ADD)),
+     12),
+])
 
-        (invoke(
-            CHAIN,
-            TEMPORARY_NUMBERS,
-            invoke(
-                PARTIAL,
-                MAP,
-                function(
-                    [TEMPORARY_NUMBER],
-                    invoke(
-                        MULTIPLY,
-                        TEMPORARY_NUMBER,
-                        integer(2)))),
-            invoke(
-                PARTIAL,
-                MAP,
-                function(
-                    [TEMPORARY_NUMBER],
-                    invoke(
-                        ADD,
-                        TEMPORARY_NUMBER,
-                        integer(1)))),
-            invoke(
-                PARTIAL,
-                MAP,
-                function(
-                    [TEMPORARY_NUMBER],
-                    invoke(
-                        SUBTRACT,
-                        TEMPORARY_NUMBER,
-                        integer(1)))),
-            invoke(
-                PARTIAL,
-                FILTER,
-                function(
-                    [TEMPORARY_NUMBER],
-                    invoke(
-                        ARE_EQUAL,
-                        invoke(
-                            MODULO,
-                            TEMPORARY_NUMBER,
-                            integer(4)),
-                        integer(0)))),
-            invoke(
-                PARTIAL,
-                REDUCE,
-                ADD)),
-         12)
-
-    ])
--- tests/integration/standard_library/type_conversion_test.py
+++ tests/integration/standard_library/type_conversion_test.py
@@ -3,25 +3,24 @@ from tests.syntax import *
 from limp.standard_library.conversions import *
 
 
-def test():
-    Helpers.run_evaluation_test_on([
-        (invoke(STRING, integer(123)),  "123"),
-        (invoke(STRING, float_(0.1)),   "0.1"),
-        (invoke(STRING, boolean(True)), "True"),
-        (invoke(STRING, string("foo")), "foo"),
+t0 = Helpers.evaluation_fixture("test", [
+    (invoke(STRING, integer(123)),  "123"),
+    (invoke(STRING, float_(0.1)),   "0.1"),
+    (invoke(STRING, boolean(True)), "True"),
+    (invoke(STRING, string("foo")), "foo"),
 
-        (invoke(INTEGER, string("32")), 32),
-        (invoke(INTEGER, string("-1")), -1),
-        (invoke(INTEGER, integer(10)),  10),
+    (invoke(INTEGER, string("32")), 32),
+    (invoke(INTEGER, string("-1")), -1),
+    (invoke(INTEGER, integer(10)),  10),
 
-        (invoke(FLOAT, string("5.2")), 5.2),
-        (invoke(FLOAT, string("0.3")), 0.3),
-        (invoke(FLOAT, float_(9.99)),  9.99),
+    (invoke(FLOAT, string("5.2")), 5.2),
+    (invoke(FLOAT, string("0.3")), 0.3),
+    (invoke(FLOAT, float_(9.99)),  9.99),
 
-        (invoke(BOOLEAN, string("false")), False),
-        (invoke(BOOLEAN, string("true")),  True),
-        (invoke(BOOLEAN, integer(1)),      True),
-        (invoke(BOOLEAN, integer(0)),      False),
-        (invoke(BOOLEAN, boolean(True)),   True),
-        (invoke(BOOLEAN, boolean(False)),  False),
-    ])
+    (invoke(BOOLEAN, string("false")), False),
+    (invoke(BOOLEAN, string("true")),  True),
+    (invoke(BOOLEAN, integer(1)),      True),
+    (invoke(BOOLEAN, integer(0)),      False),
+    (invoke(BOOLEAN, boolean(True)),   True),
+    (invoke(BOOLEAN, boolean(False)),  False),
+])
