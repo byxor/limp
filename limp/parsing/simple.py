80baacc0677225e7b884aa7d0c65146a7cbc230d
byxor <brandnewbyxor@gmail.com>
2018-07-30 23:08:38 +0100

    Refactor simple syntax tree nodes

--- limp/parsing/simple.py
+++ limp/parsing/simple.py
@@ -1,57 +1,40 @@
 import limp.tokens as Tokens
+import functools
 from limp.parsing.utils import *
 
 
-def float_node(tokens):
-    if tokens[0].type_ == Tokens.Types.Float:
-        return Node(_numeric_tree(Types.Float, tokens[0]), 1)
-
-
-def integer_node(tokens):
-    if tokens[0].type_ == Tokens.Types.Integer:
-        return Node(_numeric_tree(Types.Integer, tokens[0]), 1)
-
-
-def hexadecimal_node(tokens):
-    if tokens[0].type_ == Tokens.Types.Hexadecimal:
-        return Node(_numeric_tree(Types.Hexadecimal, tokens[0]), 1)
-
-
-def octal_node(tokens):
-    if tokens[0].type_ == Tokens.Types.Octal:
-        return Node(_numeric_tree(Types.Octal, tokens[0]), 1)
-
-
-def binary_node(tokens):
-    if tokens[0].type_ == Tokens.Types.Binary:
-        return Node(_numeric_tree(Types.Binary, tokens[0]), 1)
-
-
-def _numeric_tree(tree_type, token):
-    sign = token.contents[0]
+def _apply_sign(node):
+    tree = node[0]
+    contents = tree[1]
+    sign = contents[0]
     if sign == "+":
-        return (Types.UnaryPositive, (tree_type, token.contents[1:]))
+        return Node((Types.UnaryPositive, (tree[0], contents[1:])), 1)
     elif sign == "-":
-        return (Types.UnaryNegative, (tree_type, token.contents[1:]))
+        return Node((Types.UnaryNegative, (tree[0], contents[1:])), 1)
     else:
-        return (tree_type, token.contents)
+        return node
+            
+
+def _node(mutator, tree_type, token_type):
+    def internal(tokens):
+        if tokens[0].type_ == token_type:
+            tree = (tree_type, tokens[0].contents)
+            node = Node(tree, 1)
+            return mutator(node)
+    return internal
 
 
-def string_node(tokens):
-    if tokens[0].type_ == Tokens.Types.String:
-        return Node((Types.String, tokens[0].contents), 1)
+_basic_node = functools.partial(_node, lambda n: n)
+_numeric_node = functools.partial(_node, _apply_sign)
 
 
-def boolean_node(tokens):
-    if tokens[0].type_ == Tokens.Types.Boolean:
-        return Node((Types.Boolean, tokens[0].contents), 1)
+float_node       = _numeric_node(Types.Float, Tokens.Types.Float)
+octal_node       = _numeric_node(Types.Octal, Tokens.Types.Octal)
+binary_node      = _numeric_node(Types.Binary, Tokens.Types.Binary)
+integer_node     = _numeric_node(Types.Integer, Tokens.Types.Integer)
+hexadecimal_node = _numeric_node(Types.Hexadecimal, Tokens.Types.Hexadecimal)
 
-
-def symbol_node(tokens):
-    if tokens[0].type_ == Tokens.Types.Symbol:
-        return Node((Types.Symbol, tokens[0].contents), 1)
-
-
-def object_delimiter_node(tokens):
-    if tokens[0].type_ == Tokens.Types.ObjectDelimiter:
-        return Node((Types.Symbol, tokens[0].contents), 1)
+string_node           = _basic_node(Types.String, Tokens.Types.String)
+boolean_node          = _basic_node(Types.Boolean, Tokens.Types.Boolean)
+symbol_node           = _basic_node(Types.Symbol, Tokens.Types.Symbol)
+object_delimiter_node = _basic_node(Types.ObjectDelimiter, Tokens.Types.ObjectDelimiter)
