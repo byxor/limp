6d623c6ef74858ebad033247286f7b101ee8eb9d
byxor <brandnewbyxor@gmail.com>
2018-08-03 14:41:02 +0100

    Clean up recursion test

--- tests/integration/recursion_test.py
+++ tests/integration/recursion_test.py
@@ -1,47 +1,34 @@
 import limp
 import limp.environment as Environment
 from tests.syntax import *
-from limp.standard_library import *
 from limp.standard_library.math import *
 from limp.standard_library.comparisons import *
-from unittest import TestCase
+from nose.tools import assert_equal
 
 
-class TailCallRecursion(TestCase):
-    FUNCTION_NAME = "factorial"
-    ARGUMENT_NAME = "n"
+def test_basic_recursion():
+    function_name = "factorial"
+    parameter_name = "n"
+    implementation = function([parameter_name],
+                              if_statement(invoke(GREATER_THAN, parameter_name, integer(1)),
+                                           invoke(MULTIPLY,
+                                                  parameter_name,
+                                                  invoke(function_name,
+                                                         invoke(SUBTRACT, parameter_name, integer(1)))),
+                                           integer(1)))
 
-    IMPLEMENTATION = function(
-        [ARGUMENT_NAME],
-        if_statement(
-            invoke(GREATER_THAN, ARGUMENT_NAME, integer(1)),
-            invoke(
-                MULTIPLY,
-                ARGUMENT_NAME,
-                invoke(
-                    FUNCTION_NAME,
-                    invoke(SUBTRACT, ARGUMENT_NAME, integer(1))
-                )
-            ),
-            integer(1)
-        )
-    )
+    environment = Environment.create_standard()
+    environment.define(function_name, limp.evaluate(implementation, environment))
 
-    def setUp(self):
-        self.environment = Environment.create_standard()
-        self.evaluate = lambda code: limp.evaluate(code, self.environment)
-        self.environment.define(TailCallRecursion.FUNCTION_NAME,
-                                self.evaluate(TailCallRecursion.IMPLEMENTATION))
-
-    def test(self):
-        data = [
-            (1, 1),
-            (2, 2),
-            (3, 6),
-            (4, 24),
-            (5, 120),
-            (6, 720),
-        ]
-        for parameter, expected_output in data:
-            output = self.evaluate(invoke(TailCallRecursion.FUNCTION_NAME, str(parameter)))
-            self.assertEqual(expected_output, output)
+    data = [
+        (1, 1),
+        (2, 2),
+        (3, 6),
+        (4, 24),
+        (5, 120),
+        (6, 720),
+    ]
+    for parameter, expected_result in data:
+        source_code = invoke(function_name, integer(parameter))
+        result = limp.evaluate(source_code, environment)
+        yield assert_equal, expected_result, result
