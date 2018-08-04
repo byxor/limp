e45790c3733c12d8151ea856619999669f419aaf
byxor <brandnewbyxor@gmail.com>
2018-08-04 22:37:19 +0100

    Remove some unused code :(

--- limp/errors.py
+++ limp/errors.py
@@ -22,26 +22,3 @@ class RedefinedSymbol(LimpError):
 class EmptyCode(LimpError):
     def __init__(self):
         super().__init__("There is no code here to run.")
-
-
-class EmptyInvocation(LimpError):
-    def __init__(self):
-        message = "You cannot invoke nothing.\n\n"
-        message += "* Wrapping a function in parentheses will invoke it.\n"
-        message += "* Did you write '()' by mistake?"
-        super().__init__(message)
-
-
-def _parentheses_message(amount, quantifier):
-    message = "There {} {} too {} closing parentheses."
-    if amount == 1:
-        message = message.format("is", amount, quantifier)
-    else:
-        message = message.format("are", amount, quantifier)
-    return message
-
-
-class UnclosedString(LimpError):
-    def __init__(self):
-        message = "You forgot to close a string literal."
-        super().__init__(message)
